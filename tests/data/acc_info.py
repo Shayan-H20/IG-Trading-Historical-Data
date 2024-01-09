9e79dd98f1c3bce31a0502ee2b68d7ccea56874d
Shayan-H20 <shayanmahinfar@gmail.com>
2024-01-09 11:44:41 +0000

    mock acc_info added for testing

---
+++ tests/data/acc_info.py
@@ -0,0 +1,34 @@
+acc_info_mock = {
+    "accountType": "SPREADBET",
+    "accountInfo": {
+        "balance": 1_000_000.0,
+        "deposit": 0.0,
+        "profitLoss": 0.0,
+        "available": 1_000_000.0,
+    },
+    "currencyIsoCode": "GBP",
+    "currencySymbol": "£",
+    "currentAccountId": "ABC_SB",
+    "lightstreamerEndpoint": "https://demo-apd.marketdatasystems.com",
+    "accounts": [
+        {
+            "accountId": "ABC_SB",
+            "accountName": "MockAccount-SB",
+            "preferred": True,
+            "accountType": "SPREADBET",
+        },
+        {
+            "accountId": "ABC",
+            "accountName": "MockAccount-CFD",
+            "preferred": False,
+            "accountType": "CFD",
+        },
+    ],
+    "clientId": "123456789",
+    "timezoneOffset": 0,
+    "hasActiveDemoAccounts": True,
+    "hasActiveLiveAccounts": True,
+    "trailingStopsEnabled": True,
+    "reroutingEnvironment": None,
+    "dealingEnabled": True,
+}
