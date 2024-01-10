1389123af3e3c579748477a5bc6ee30a5981d2ea
Shayan-H20 <shayanmahinfar@gmail.com>
2024-01-10 16:38:55 +0000

    test for login complete

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,24 +1,46 @@
 from ig_trading_historical_data import IG_API
-import user_info
-import pytest
+import responses
+import json
 
 
-#@pytest.fixture
-#def logged_in_instance():
-#    pass
+""" unit tests for login """
 
-def test_can_login_via_init():
-    # user info to log in
-    DEMO = 1
-    username = user_info.username_demo
-    pw = user_info.pw_demo
-    api_key = user_info.api_key_demo
 
-    ig_api = IG_API(DEMO, username, pw, api_key)
+class TestLogin:
     
-    assert ig_api.acc_info.keys()
+    # initialize class variables
+    mock_user_info_demo = {
+        "DEMO": 1,
+        "username": "test_username",
+        "pw": "test_pw",
+        "api_key": "test_api_key",
+    }
 
 
+    @responses.activate
+    def test_can_login_demo_via_init(self):
+        
+        with open("tests/data/mock_acc_info.json", "r") as f:
+            mock_response_json = json.load(f)
 
+        # mock the request.post inside the API's class initialization
+        mock_url = "https://demo-api.ig.com/gateway/deal/session"
+        mock_response_headers = {
+            "CST": "test_CST",
+            "X-SECURITY-TOKEN": "test_X-SECURITY-TOKEN"
+        }
 
-pytest.main()
+        responses.post(
+            mock_url,
+            json=mock_response_json,
+            status=200,
+            headers=mock_response_headers,
+        )
+        
+        ig_api = IG_API(**self.mock_user_info_demo)
+        
+        assert ig_api.acc_info['accounts'][0]['accountName'] == mock_response_json['accounts'][0]['accountName']
+        assert ig_api.acc_info['accounts'][1]['accountName'] == mock_response_json['accounts'][1]['accountName']
+        assert ig_api.acc_info['clientId'] == mock_response_json['clientId']
+        assert ig_api.acc_info['accountInfo']['balance'] == mock_response_json['accountInfo']['balance']
+        assert ig_api.header_base['CST'] == mock_response_headers['CST']
