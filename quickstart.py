e44d7da4d9957c8e65b410022d32ae21dce29d65
Shayan-H20 <shayanmahinfar@gmail.com>
2024-01-11 19:31:49 +0000

    quickstart complete

--- quickstart.py
+++ quickstart.py
@@ -4,67 +4,50 @@ import user_info
 from pprint import pprint  # for nicer dictionary printing
 
 
-# user input
-DEMO = 1
+""" input user information """
+
+# account details
+DEMO = 1  # 1: using demo account / 0: using live account
 username = user_info.username
 pw = user_info.pw
 api_key = user_info.api_key
 
-# format of the 'assets' dictionary with example data:
+# let's say you want data for 'Microsoft' and 'GBPUSD Forward'
+# format of the 'assets' dictionary:
 assets = {
     
     'GBPUSD Forward': {  # asset name in normal language (without slashes)
-        'instrumentName': 'GBP/USD Forward',  # asset name in EXACT way seen on 
-                                              # IG web platform (with slashes if relevant)
+        'instrumentName': 'GBP/USD Forward',  # asset name in EXACT way as seen on IG web platform (with slashes if relevant)
         'expiry': 'MAR-24'  # either 'DFB' or the expiration date
     },
-    
-    'Tesla': {  # another asset example
-        'instrumentName': 'Tesla Motors Inc (All Sessions)',  
+
+    'Microsoft': {  # another asset example
+        'instrumentName': 'Microsoft Corp (All Sessions)',  
         'expiry': 'DFB'
     },
 }
 
-# historical data details
-resolution = 'MINUTE_3'  # price resolution (SECOND, MINUTE, MINUTE_2, MINUTE_3, MINUTE_5, MINUTE_10, MINUTE_15, MINUTE_30, HOUR, HOUR_2, HOUR_3, HOUR_4, DAY, WEEK, MONTH)
-rangeType = 'numPoints'  # 'numPoints' or 'dates'
-numPoints = 1
-startDate = '2023-10-25 00:00:00'  # yyyy-MM-dd HH:mm:ss (inclusive dates and times)
-endDate = '2023-11-28 00:00:00'  # yyyy-MM-dd HH:mm:ss (inclusive dates and times)
-weekdays = (0, 1, 2, 3, 4, 5)  # 0: Mon, 6: Sun (deactivated if time portion above is equal) 
-
-"""API HISTORICAL DATA LIMITS (INDICATION)
-------------------------------------------
-Resolution	    Days
-1 Sec	        4
-1 Min	        40
-2 Min	        40
-3 Min	        40
-5 Min	        360
-10 Min	        360
-15 Min	        360
-30 Min	        360
-1 Hour	        360
-2 Hour	        360
-3 Hour	        360
-4 Hour	        360
-1 Day	        15 years
-------------------------------------------
-"""
+# historical data inputs
+resolution = 'MINUTE_5'  # price resolution (SECOND, MINUTE, MINUTE_2, MINUTE_3, MINUTE_5, MINUTE_10, MINUTE_15, MINUTE_30, HOUR, HOUR_2, HOUR_3, HOUR_4, DAY, WEEK, MONTH)
+rangeType = 'dates'  # 'numPoints' or 'dates'
+numPoints = 10  # ignored if rangeType == 'dates'
+startDate = '2024-01-08 10:00:00'  # yyyy-MM-dd HH:mm:ss (inclusive dates and times)
+endDate = '2024-01-10 10:30:00'  # yyyy-MM-dd HH:mm:ss (inclusive dates and times)
+weekdays = (0, 2)  # 0: Mon, 6: Sun (deactivated if time portion above is equal) 
 
 
+""" API key usage """
 
-# ---------------------------------------------------------------
-# API USAGE
-# ---------------------------------------------------------------
-# logging in
+# logging in / make a class instance
 ig_api = IG_API(DEMO, username, pw, api_key)
 
-# update 'assets' dict with respective epics
+# get epics automatically and update 'assets' dict with respective epics
 assets = ig_api.get_epics(assets)
-pprint(assets)
 
-# retrieve historical data
+# view epics
+pprint(assets) 
+
+# get historical prices
 assets, allowance = ig_api.get_prices_all_assets(
     assets, 
     resolution, 
@@ -74,28 +57,20 @@ assets, allowance = ig_api.get_prices_all_assets(
     weekdays,
     numPoints
 )
-pprint(assets)
+
+# view data fields
+print(assets['GBPUSD Forward']['prices'].columns)
+
+# view pricing data (17 columns of data fields)
+print(assets['GBPUSD Forward']['prices'])
+print(assets['Microsoft']['prices'])
+
+
+""" sample of other available values """
+
+# view remaining quota for the week
 pprint(allowance)
 
-# example outputs
-pprint(assets.keys())
-
-pprint(assets['GBPUSD Forward'].keys())
-
-pprint(assets['GBPUSD Forward']['instrumentName'])
-pprint(assets['Tesla']['instrumentName'])
-
-pprint(assets['GBPUSD Forward']['expiry'])
-pprint(assets['Tesla']['expiry'])
-
-pprint(assets['GBPUSD Forward']['epic'])
-pprint(assets['Tesla']['epic'])
-
+# view instrument types
 pprint(assets['GBPUSD Forward']['instrumentType'])
-pprint(assets['Tesla']['instrumentType'])
-
-pprint(assets['GBPUSD Forward']['prices'])
-pprint(assets['Tesla']['prices'])
-
-pprint(assets['GBPUSD Forward']['prices'][['closePxMid', 'lastTradedVolume']])
-pprint(assets['Tesla']['prices'][['closePxMid', 'lastTradedVolume']])
+pprint(assets['Microsoft']['instrumentType'])
